92bbc9800a969843126d0f8d6f1097eca0c30ba7
Himeko-CodeGeass <nf2ct9z5mn@privaterelay.appleid.com>
2026-09-05 22:57:53 +0800

    Update test_gestalt_engine.py

--- tests/test_gestalt_engine.py
+++ tests/test_gestalt_engine.py
@@ -2,7 +2,8 @@ import sys
 import os
 
 # 強制將專案根目錄加入 Python 搜尋路徑
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath('.'))
+
 
 import unittest
 from core.gestalt_engine import GestaltEngine
